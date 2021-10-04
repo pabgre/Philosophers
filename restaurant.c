f0683434fe1129f86102222fd617a97702f19d7d
pabgre <pablosan@tutanota.com>
2021-10-04 11:51:58 +0200

    Philosophers Done

--- restaurant.c
+++ restaurant.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   restaurant.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psan-gre <psan-gre@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: psan-gre <psan-gre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 12:17:01 by psan-gre          #+#    #+#             */
-/*   Updated: 2021/09/29 14:03:27 by psan-gre         ###   ########.fr       */
+/*   Updated: 2021/10/04 11:39:27 by psan-gre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,15 @@ int	notify_state(t_mutex *print_lock, t_philo philo, t_state state)
 	time_stamp = get_timestamp() - philo.params.init_time;
 	pthread_mutex_lock(print_lock);
 	if (state == _fork_taken)
-		printf("[%ld] %d has taken a fork.\n", time_stamp, philo.id);
+		printf("[%llu] %d has taken a fork.\n", time_stamp, philo.id);
 	if (state == _eating)
-		printf("[%ld] %d is eating.\n", time_stamp, philo.id);
+		printf("[%llu] %d is eating.\n", time_stamp, philo.id);
 	if (state == _sleeping)
-		printf("[%ld] %d is sleeping.\n", time_stamp, philo.id);
+		printf("[%llu] %d is sleeping.\n", time_stamp, philo.id);
 	if (state == _thinking)
-		printf("[%ld] %d is thinking.\n", time_stamp, philo.id);
+		printf("[%llu] %d is thinking.\n", time_stamp, philo.id);
 	if (state == _died)
-		printf("[%ld] %d died.\n", time_stamp, philo.id);
+		printf("[%llu] %d died.\n", time_stamp, philo.id);
 	if (state != _died)
 		pthread_mutex_unlock(print_lock);
 	return (0);
